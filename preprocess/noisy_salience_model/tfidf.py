447f953a7a34560097b095c48ab5914c210069f3
blodstone <hardy.oei@gmail.com>
2020-01-21 07:14:58 +0000

    Bug: Misformula in tfidf

--- preprocess/noisy_salience_model/tfidf.py
+++ preprocess/noisy_salience_model/tfidf.py
@@ -5,25 +5,22 @@ from noisy_salience_model.salience_model import Dataset, SalienceSet
 
 
 def process(dataset: Dataset) -> Dataset:
-    tfidf = dict()
-    tf_count = Counter()
-    idf_count = dict()
+    idf = dict()
+    tf_count_all = Counter()
+    tf_count = dict()
     for doc_id, instance in dataset.items():
-        idf_count[doc_id] = Counter()
+        tf_count[doc_id] = Counter()
         for line in instance.doc:
-            tf_count.update(line)
-            idf_count[doc_id].update(line)
-    for word, tf in tf_count.items():
-        idf = math.log(
-            len(idf_count) /
-            (1 + sum([1 for _, counter in idf_count.items() if counter[word] > 0]))) + 1
-        if tf != 0:
-            tf = math.log(1 + tf)
-        tfidf[word] = tf * idf
+            tf_count_all.update(line)
+            tf_count[doc_id].update(line)
+    for word, tf in tf_count_all.items():
+        idf[word] = math.log(
+            len(tf_count) /
+            (1 + sum([1 for _, counter in tf_count.items() if counter[word] > 0])), 2) + 1
     for doc_id, instance in dataset.dataset.items():
         salience = SalienceSet.init_salience_set(instance.doc_size)
         for i, line in enumerate(instance.doc):
             for j, word in enumerate(line):
-                salience[i][j] = tfidf[word]
+                salience[i][j] = idf[word] * math.log(tf_count[doc_id][word], 2)
         dataset[doc_id].salience_set['tfidf'] = salience
     return dataset
