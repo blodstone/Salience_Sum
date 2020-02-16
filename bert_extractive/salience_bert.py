da08e0a0965f7e253a51b813660149a953fefbaf
blodstone <hardy.oei@gmail.com>
2020-02-16 00:45:46 +0000

    Change: Oracle using BERTScore New: Extractive summarization using unsupervised models salience

--- bert_extractive/salience_bert.py
+++ bert_extractive/salience_bert.py
@@ -38,60 +38,67 @@ nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
 
 
 def read_text(lines):
-    salience_summs = []
+    oracle_summs = []
     for i, line in enumerate(lines):
         print(f'Process {i}')
         if line.strip() == '':
             continue
         oracle_summ = process_doc(line)
-        salience_summs.append(oracle_summ)
-    return salience_summs
+        oracle_summs.append(oracle_summ)
+    return oracle_summs
 
 
 def process_doc(line):
     src_seq, tgt_seq = line.split('\t')
     collection_seq = list(zip(*[group.split(u'￨') for group in src_seq.split()]))
     src_seq = ' '.join(collection_seq[0])
+    salience_seqs = [[float(value) for value in seq] for seq in collection_seq[1:]]
     doc = segmenter(src_seq)
-    tgt = nlp(tgt_seq)
-    # Assuming 1 sentence gold summary for each document (BBC only).
-    # For CNN/DM, it will need to be rewrote
-    if is_using_gpu:
-        t_tensor = from_dlpack(tgt.tensor.toDlpack())
-    else:
-        t_tensor = torch.tensor(tgt.tensor)
-    doc_sims = []
+    sum_t_tensor = torch.zeros(1, 768)
+    j = 0
+    sent_nlps = []
     for sent in doc.sents:
-        # We re-parse each sentence to limit the context to the sentence.
-        src = nlp(sent.text)
+        sent_nlp = nlp(sent.text)
+        sent_nlps.append(sent_nlp)
         if is_using_gpu:
-            src_tensor = from_dlpack(src.tensor.toDlpack())
+            sent_nlp_tensor = from_dlpack(sent_nlp.tensor.toDlpack())
         else:
-            src_tensor = torch.tensor(src.tensor)
-        result_tensor = torch.zeros(src_tensor.size(0), t_tensor.size(0))
-        for i, s in enumerate(src_tensor.split(1, 0)):
-            for j, t in enumerate(t_tensor.split(1, 0)):
-                result_tensor[i, j] = s.squeeze().dot(t.squeeze())
-        recall = result_tensor.max(dim=0)[0].sum() / t_tensor.size(0)
-        precision = result_tensor.max(dim=1)[0].sum() / src_tensor.size(0)
-        f1 = 2 * (recall * precision) / (recall + precision)
-        doc_sims.append(f1.item())
-    sent_idx = [(i, j, len(list(doc.sents)[i])) for i, j in enumerate(doc_sims)]
+            sent_nlp_tensor = torch.tensor(sent_nlp.tensor)
+        for i, salience_model in enumerate(salience_seqs):
+            token_j = j
+            for token in sent_nlp:
+                if not token.is_stop:
+                    sum_t_tensor += sent_nlp_tensor[i, :] * salience_model[j]
+                    token_j += 1
+        j = token_j
+    doc_sims = []
+    for sent_nlp in sent_nlps:
+        if is_using_gpu:
+            sent_nlp_tensor = from_dlpack(sent_nlp.tensor.toDlpack())
+        else:
+            sent_nlp_tensor = torch.tensor(sent_nlp.tensor)
+        sent_sims = []
+        for t in sent_nlp_tensor.split(1, 0):
+            similarity = t.squeeze().dot(sum_t_tensor.squeeze())
+            sent_sims.append(similarity.item())
+        doc_sims.append(sent_sims)
+    means = [statistics.mean(sims) for sims in doc_sims]
+    sent_idx = [(i, j, len(list(doc.sents)[i])) for i, j in enumerate(means)]
     sent_idx.sort(key=lambda x: x[1], reverse=True)
-    oracle_summ = ''
+    summ = ''
     for i, _, l in sent_idx:
         if l <= 100:
-            oracle_summ = list(doc.sents)[i].text
+            summ = list(doc.sents)[i].text
             break
-    return oracle_summ
+    return summ
 
 
 def main():
     input_path = Path(args.input)
     output_path = Path(args.output)
     lines = input_path.open('r').readlines()
-    oracle_summs = read_text(lines)
-    (output_path / 'oracle.txt').write_text('\n'.join(oracle_summs))
+    summs = read_text(lines)
+    (output_path / 'salience_out.txt').write_text('\n'.join(summs))
 
 
 if __name__ == '__main__':
