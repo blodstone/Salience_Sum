dca5e62b8a59cb877ac30acb26d54ccbb760911d
Hardy <hardy.oei@gmail.com>
2020-02-20 12:20:25 +0000

    Bug: Typo

--- HPC/sharc/simple_script/run_simple.py
+++ HPC/sharc/simple_script/run_simple.py
@@ -19,7 +19,7 @@ def main():
             jobids[jobid] = i
         for jobid, i in jobids.items():
             retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
-            result = list(open(f'simple_{i}'.csv).readlines())[0]
+            result = list(open(f'simple_{i}.csv').readlines())[0]
             print(f'{retval.jobId} has finished with status {retval.hasExited}')
             output.append(result)
         s.deleteJobTemplate(jt)
