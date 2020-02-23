9d4a01e875e51afbc52682b5b788272ede764f1c
blodstone <hardy.oei@gmail.com>
2020-02-23 00:07:53 +0000

    Add: Oracle bertscore extraction for CNN

---
+++ HPC/BERT/oracle_bert_cnn.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#$ -l gpu=1
+#$ -l rmem=24G
+#$ -l h_rt=48:00:00
+#$ -M hhardy2@sheffield.ac.uk
+#$ -m easb
+#$ -j y
+#$ -o oracle_bert_cnn.txt
+#$ -wd /home/acp16hh/Salience_Sum
+
+python bert_extractive/oracle.py -input /data/acp16hh/data/cnn/ready/test.tsv -output /data/acp16hh/data/cnn/result -dataset CNN
