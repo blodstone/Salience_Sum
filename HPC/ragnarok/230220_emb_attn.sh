6a105b6cb9a4bcefaf124639ee5c5b83b1486e54
blodstone <hardy.oei@gmail.com>
2020-02-23 23:40:40 +0000

    New: Seed 200 Ragnarok embedding attention

---
+++ HPC/ragnarok/230220_emb_attn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export RANDOM_SEED=200
+export NUMPY_SEED=200
+export PYTORCH_SEED=200
+export CUDA_VISIBLE_DEVICES=0
+HOME=/home/acp16hh/Projects/Research/Experiments/Exp_Gwen_Saliency/src/SalienceSum
+MODEL=$HOME/data/output/pg_salience_feature/bbc/dgx/salience_emb_attn_16/200
+DATA=$HOME/data/bbc
+source $HOME/venv/bin/activate
+export train_path=$DATA/ready/train.salience.tsv
+export validation_path=$DATA/ready/validation.salience.tsv
+allennlp train -s $MODEL -f --file-friendly-logging --include-package pg_salience_feature $HOME/HPC/pg_salience_feature/pg_salience_emb_mlp_attn_16.jsonnet
+python $HOME/postprocess/retrieve_last_model.py $MODEL
+python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/pg_salience_emb_mlp_attn_16.jsonnet -output_path $DATA/result/test_pg_salience_feature_bbc_dgx_salience_emb_attn_16.out -batch_size 24
