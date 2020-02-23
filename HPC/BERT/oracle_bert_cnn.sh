b6ed46c9110bfecba6ea17305127d17b16e607b8
blodstone <hardy.oei@gmail.com>
2020-02-23 10:05:06 +0000

    Bug: Need to activate python3

--- HPC/BERT/oracle_bert_cnn.sh
+++ HPC/BERT/oracle_bert_cnn.sh
@@ -7,5 +7,7 @@
 #$ -j y
 #$ -o oracle_bert_cnn.txt
 #$ -wd /home/acp16hh/Salience_Sum
+module load apps/python/conda
+source activate gwen
 
 python bert_extractive/oracle.py -input /data/acp16hh/data/cnn/ready/test.tsv -output /data/acp16hh/data/cnn/result -dataset CNN
