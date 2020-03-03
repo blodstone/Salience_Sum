d9ca57fe96e6793b94210660d7d0f23d01c9ff3b
blodstone <hardy.oei@gmail.com>
2020-03-03 14:26:27 +0000

    Bug: Specify DRMAA path

--- HPC/preprocess/run_cnn_pkusumsum.sh
+++ HPC/preprocess/run_cnn_pkusumsum.sh
@@ -7,5 +7,5 @@
 
 module load apps/python/conda
 source activate gwen
-
-python HPC/parallel_split_cnn_pkusumsum.py -input_path /data/acp16hh/data/cnn/raw/docs -tmp_path /data/acp16hh/data/cnn/intermediary/raw -pkusumsum_path /home/acp16hh/PKUSUMSUM -n 75000
+DRMAA_LIBRARY_PATH=/usr/local/sge/live/lib/lx-amd64/libdrmaa.so; export DRMAA_LIBRARY_PATH
+python HPC/parallel_split_cnn_pkusumsum.py -input_path /data/acp16hh/data/cnn/raw/docs -tmp_path /data/acp16hh/data/cnn/intermediary/raw -pkusumsum_path /home/acp16hh/PKUSUMSUM -n 1000
