b286c25e79b3e8bbb4e99c599cb30df6b640e0db
blodstone <hardy.oei@gmail.com>
2020-03-09 22:38:46 +0000

    Add: Missing scripts

--- tests/pointer_generator.py
+++ tests/pointer_generator.py
@@ -12,7 +12,7 @@ class TestPointerGeneratorModel:
                    shared_datadir: Path, datadir: Path):
         param_file = shared_datadir / 'model_config' / 'pointer_generator.jsonnet'
         dataset_file = {
-            'train': '/home/acp16hh/Projects/Research/Experiments/Exp_Gwen_Saliency_Summ/src/Salience_Sum/data/bbc_dev/ready/train.concat.tsv',
+            'train': '/home/acp16hh/Projects/Research/Experiments/Exp_Gwen_Saliency_Summ/src/Salience_Sum/data/cnndm/ready/test.dev.tsv',
             'validation': '/home/acp16hh/Projects/Research/Experiments/Exp_Gwen_Saliency_Summ/src/Salience_Sum/data/bbc_dev/ready/validation.concat.tsv'
         }
         my_model = model(param_file, dataset_file, datadir)
