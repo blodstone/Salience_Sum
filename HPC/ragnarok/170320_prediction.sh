3a392177aa115fc8264178ceb38e742bdfa91926
blodstone <hardy.oei@gmail.com>
2020-03-16 22:22:03 +0000

    Bug: Wrong seed path

--- HPC/ragnarok/170320_prediction.sh
+++ HPC/ragnarok/170320_prediction.sh
@@ -18,4 +18,4 @@ python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODE
 
 MODEL=$HOME/model/bbc/seq2seq_emb_mlp_attn_16/300
 python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $MODEL/config.json -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_200.out -batch_size 24 --cuda --use_salience
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $MODEL/config.json -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_300.out -batch_size 24 --cuda --use_salience
