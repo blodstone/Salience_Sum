e35018d187b0bb0ffdc1f66a2d1eb014fde2bb64
blodstone <hardy.oei@gmail.com>
2020-03-16 22:18:50 +0000

    Bug: Revise the prediction script

--- HPC/ragnarok/170320_prediction.sh
+++ HPC/ragnarok/170320_prediction.sh
@@ -6,20 +6,16 @@ source $HOME/venv/bin/activate
 
 MODEL=$HOME/model/bbc/seq2seq_emb_mlp_16/200
 python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_200.out -batch_size 24 --cuda
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_200.out -batch_size 24 --cuda --use_salience
 
 MODEL=$HOME/model/bbc/seq2seq_emb_mlp_16/300
 python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_300.out -batch_size 24 --cuda
-
-MODEL=$HOME/model/bbc/seq2seq_emb_mlp_attn_16/100
-python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp_attn.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_100.out -batch_size 24 --cuda
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_300.out -batch_size 24 --cuda --use_salience
 
 MODEL=$HOME/model/bbc/seq2seq_emb_mlp_attn_16/200
 python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp_attn.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_200.out -batch_size 24 --cuda
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp_attn.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_200.out -batch_size 24 --cuda --use_salience
 
 MODEL=$HOME/model/bbc/seq2seq_emb_mlp_attn_16/300
 python $HOME/postprocess/retrieve_last_model.py $MODEL
-python $HOME/summarize.py -module pg_salience_feature -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp_attn.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_200.out -batch_size 24 --cuda
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $HOME/HPC/pg_salience_feature/batch_16/seq2seq_salience_emb_mlp_attn.jsonnet -output_path $DATA/result_3/seq2seq_salience_emb_mlp_attn_200.out -batch_size 24 --cuda --use_salience
