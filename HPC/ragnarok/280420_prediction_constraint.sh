9f2f0a23e07e35df14e2590ffed177d057277326
blodstone <hardy.oei@gmail.com>
2020-04-28 01:16:20 +0100

    Add: Script for std and constraint

---
+++ HPC/ragnarok/280420_prediction_constraint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+HOME=/home/acp16hh/Projects/Research/Experiments/Exp_Gwen_Saliency/src/SalienceSum
+DATA=$HOME/data/bbc
+
+source $HOME/venv/bin/activate
+
+MODEL=$HOME/model/bbc/pg_clean_16/100
+CONFIG=$HOME/model_config/pg_clean_16/constraint_beam/100
+python $HOME/summarize.py -input $DATA/ready/test.constraint.10.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/constraint_pg_clean_100.out -batch_size 24 --cuda --use_salience --use_constraint
+
+MODEL=$HOME/model/bbc/pg_clean_16/200
+CONFIG=$HOME/model_config/pg_clean_16/constraint_beam/200
+python $HOME/summarize.py -input $DATA/ready/test.constraint.10.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/constraint_pg_clean_200.out -batch_size 24 --cuda --use_salience --use_constraint
+
+MODEL=$HOME/model/bbc/pg_clean_16/300
+CONFIG=$HOME/model_config/pg_clean_16/constraint_beam/300
+python $HOME/summarize.py -input $DATA/ready/test.constraint.10.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/constraint_pg_clean_300.out -batch_size 24 --cuda --use_salience --use_constraint
+
--- HPC/ragnarok/280420_prediction_std.sh
+++ HPC/ragnarok/280420_prediction_std.sh
@@ -11,9 +11,9 @@ python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODE
 
 MODEL=$HOME/model/bbc/pg_clean_16/200
 CONFIG=$HOME/model_config/pg_clean_16/std_beam/200
-python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/std_pg_clean_200.out -batch_size 24 --cuda --use_salience --use_constraint
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/std_pg_clean_200.out -batch_size 24 --cuda --use_salience
 
 MODEL=$HOME/model/bbc/pg_clean_16/300
 CONFIG=$HOME/model_config/pg_clean_16/std_beam/300
-python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/std_pg_clean_300.out -batch_size 24 --cuda --use_salience --use_constraint
+python $HOME/summarize.py -input $DATA/ready/test.salience.tsv -vocab_path $MODEL/vocabulary -model $MODEL/pick.th -model_config $CONFIG/config.json -output_path $DATA/result_4/std_pg_clean_300.out -batch_size 24 --cuda --use_salience
 
