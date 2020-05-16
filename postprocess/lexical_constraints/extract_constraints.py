bddcd6e869a31aefa848d694000ce7c29d1fb8fe
blodstone <hardy.oei@gmail.com>
2020-05-16 11:01:10 +0100

    Change: Add oracle constraint extraction

--- postprocess/lexical_constraints/extract_constraints.py
+++ postprocess/lexical_constraints/extract_constraints.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from typing import List
 
+from tqdm import tqdm
+
 
 class WhitespaceTokenizer(object):
     def __init__(self, vocab):
@@ -42,121 +44,116 @@ def subset_sum(a: List, k: float):
     while j > 0:
         if i == 0:
             break
-        if a[j-1] != i:
-            while m[j-1][i]:
+        if a[j - 1] != i:
+            while m[j - 1][i]:
                 j -= 1
-        searched_set.append(a[j-1])
-        i = i - a[j-1][1]
+        searched_set.append(a[j - 1])
+        i = i - a[j - 1][1]
         j -= 1
     return searched_set
 
 
-def knapSack(W, wt, val, n):
-    K = [[0 for w in range(W + 1)]
-         for i in range(n + 1)]
+def extract_phrases(doc_src, doc_tgt, max_len, salience_seqs, k, is_oracle):
+    doc_chunks = doc_src.noun_chunks
+    tgt_chunks = doc_tgt.noun_chunks
+    added_words_src = []
+    added_words_tgt = []
+    result = []
+    used_idx_src = []
+    used_idx_tgt = []
+    # (1) Extract noun chunks and calculate the average score of the chunks,
+    # also remove stop words from noun chunks
+    for chunk in doc_chunks:
+        phrase_idxs = []
+        score = 0
+        phrase_words = []
+        for token in chunk:
+            if not token.is_stop and token.i < max_len:
+                phrase_words.append(token.text)
+                phrase_idxs.append(token.i)
+                used_idx_src.append(token.i)
+                score += salience_seqs[token.i]
+        if len(phrase_idxs) != 0 and phrase_words not in added_words_src:
+            score /= len(phrase_idxs)
+            result.append((phrase_idxs, phrase_words, score))
+            added_words_src.append(phrase_words)
+    # (2) Extract single word with stop words and noun chunks removed from text
+    for token in doc_src:
+        if not token.is_stop and (token.i not in used_idx_src) and (token.i < max_len):
+            if [token.text] not in added_words_src:
+                result.append(([token.i], [token.text], salience_seqs[token.i]))
+                added_words_src.append([token.text])
 
-    # Build table K[][] in bottom
-    # up manner
-    for i in range(n + 1):
-        for w in range(W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i - 1] <= w:
-                K[i][w] = max(val[i - 1]
-                              + K[i - 1][w - wt[i - 1]],
-                              K[i - 1][w])
-            else:
-                K[i][w] = K[i - 1][w]
-
-                # stores the result of Knapsack
-    res = K[n][W]
-
-    w = W
-    items = []
-    for i in range(n, 0, -1):
-        if res <= 0:
-            break
-        # either the result comes from the
-        # top (K[i-1][w]) or from (val[i-1]
-        # + K[i-1] [w-wt[i-1]]) as in Knapsack
-        # table. If it comes from the latter
-        # one/ it means the item is included.
-        if res == K[i - 1][w]:
-            continue
-        else:
-
-            # This item is included.
-            items.append(wt[i - 1])
-
-            # Since this weight is included
-            # its value is deducted
-            res = res - val[i - 1]
-            w = w - wt[i - 1]
-    return items
+    if is_oracle:
+        for chunk in tgt_chunks:
+            phrase_idxs = []
+            phrase_words = []
+            for token in chunk:
+                if not token.is_stop and token.i < max_len:
+                    phrase_words.append(token.text)
+                    phrase_idxs.append(token.i)
+                    used_idx_tgt.append(token.i)
+            if len(phrase_idxs) != 0 and phrase_words not in added_words_tgt:
+                added_words_tgt.append(phrase_words)
+        for token in doc_tgt:
+            if not token.is_stop and not token.i in used_idx_tgt and token.i < max_len:
+                if [token.text] not in added_words_tgt:
+                    added_words_tgt.append([token.text])
+        new_result = []
+        for r in result:
+            if r[1] in added_words_tgt:
+                new_result.append(r)
+        result = new_result
+    if len(result) == 0:
+        return []
+    # (3) Sort and extract top k
+    result.sort(key=lambda x: x[1], reverse=True)
+    if is_oracle:
+        return result
+    else:
+        return result[:k]
 
 
 def main():
     nlp = spacy.load("en_core_web_sm")
     nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
     k = args.k
+    is_oracle = args.oracle
+    output_name = args.output_name
     max_len = args.max_len
+    if max_len == -1:
+        max_len = float('inf')
     tsv = Path(args.tsv)
     updated_lines = []
-    i = 1
-    for line in tsv.open('r'):
+    count_empty_final_set = 0
+    for line in tqdm(tsv.open('r').readlines()):
         line = line.strip()
         if line == '':
             continue
-        try:
-            print(i)
-            i += 1
-            orig_src_seq, tgt_seq = line.split('\t')
-            collection_seq = list(zip(*[group.split(u'￨') for group in orig_src_seq.split()]))
-            src_seq = collection_seq[0]
+        orig_src_seq, tgt_seq = line.split('\t')
+        collection_seq = list(zip(*[group.split(u'￨') for group in orig_src_seq.split()]))
+        src_seq = collection_seq[0]
 
-            salience_seqs = [[float(value) if float(value) <= 1.0 else 1.0 for value in seq] for seq in
-                             collection_seq[1:]]
-            salience_seqs = [sum(saliences) for saliences in zip(*salience_seqs)]
-            # (1) Extract noun chunks and calculate the average score of the chunks,
-            # also remove stop words from noun chunks
-            result = []
-            used_idx = []
-            added_words = []
-            doc = nlp(" ".join(src_seq))
-            chunks = doc.noun_chunks
-            for chunk in chunks:
-                phrase_idxs = []
-                score = 0
-                phrase_words = []
-                for token in chunk:
-                    if not token.is_stop and token.i < max_len:
-                        phrase_words.append(token.text)
-                        phrase_idxs.append(token.i)
-                        used_idx.append(token.i)
-                        score += salience_seqs[token.i]
-                if len(phrase_idxs) != 0 and phrase_words not in added_words:
-                    score /= len(phrase_idxs)
-                    result.append((phrase_idxs, score))
-                    added_words.append(phrase_words)
-            # (2) Extract single word with stop words and noun chunks removed from text
-            for token in doc:
-                if not token.is_stop and not token.i in used_idx and token.i < max_len:
-                    if [token.text] not in added_words:
-                        result.append(([token.i], salience_seqs[token.i]))
-                        added_words.append([token.text])
-            # (3) Sort and extract top k
-            result.sort(key=lambda x: x[1], reverse=True)
-            final_set = result[:k]
-            # (4) Format for writing back
-            formatted_result = []
-            for phrase, value in final_set:
-                words = u' '.join([str(idx) for idx in phrase])
-                formatted_result.append(words)
-            updated_line = f'{orig_src_seq}\t{tgt_seq}\t{u"￨".join(formatted_result)}'
-            updated_lines.append(updated_line)
-        except ValueError:
-            continue
-    (tsv.parent / f'{tsv.name.split(".")[0]}.constraint.tsv').open('w')\
+        salience_seqs = [[float(value) if float(value) <= 1.0 else 1.0 for value in seq] for seq in
+                         collection_seq[1:]]
+        salience_seqs = [sum(saliences) for saliences in zip(*salience_seqs)]
+
+        doc_src = nlp(" ".join(src_seq))
+        doc_tgt = nlp(tgt_seq)
+
+        final_set = extract_phrases(doc_src, doc_tgt, max_len, salience_seqs, k, is_oracle)
+
+        if len(final_set) == 0:
+            print(count_empty_final_set)
+            count_empty_final_set += 1
+        # (4) Format for writing back
+        formatted_result = []
+        for phrase, _, value in final_set:
+            words = u' '.join([str(idx) for idx in phrase])
+            formatted_result.append(words)
+        updated_line = f'{orig_src_seq}\t{tgt_seq}\t{u"￨".join(formatted_result)}'
+        updated_lines.append(updated_line)
+    (tsv.parent / f'{tsv.name.split(".")[0]}.{output_name}.tsv').open('w') \
         .write('\n'.join(updated_lines))
 
 
@@ -164,6 +161,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-tsv', help='TSV file.')
     parser.add_argument('-k', help='Top phrases to use.', type=int)
-    parser.add_argument('-max_len', help='Maximum length of text to consider.', type=int)
+    parser.add_argument('-max_len', help='Maximum length of text to consider.', type=int, default=-1)
+    parser.add_argument('--oracle', help='Set to oracle', action='store_true')
+    parser.add_argument('-output_name', help='The output name. The prefix is set to the input name.')
     args = parser.parse_args()
     main()
